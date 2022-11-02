1071a96852e868234e507f69c4a00a7500627639
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-02 11:03:55 +0000

    generate seed from csv data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+# reset database
+Application.delete_all
+
+# load csv
+application_csv = File.read(Rails.root.join('db','csv', 'jobapplication_seed.csv'))
+
+# create records from csv
+p 'reading csv'
+data = CSV.parse(application_csv, headers: true,encoding: 'ISO-8859-1')
+counter = 0
+data.each do |row|
+  d = Application.create(
+    date: row['date'],
+    job_title: row['job_title'],
+    company: row['company'],
+    application_method: row['application_method'],
+    response: row['response'],
+    comment: row['comment']
+  )
+  # print errors if any
+  if d.errors.size > 0
+    p ''
+    puts d.job_title
+    puts d.errors
+  end
+
+  counter += 1 if d.persisted?
+  print '.'
+end
+
+p ''
+p ">>> #{counter} #{'application'.pluralize(counter)} created"
