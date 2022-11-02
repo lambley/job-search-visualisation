60a508fdedefd52a11ab73175a71953a2c2c877f
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-02 16:32:02 +0000

    add show tests

--- server/spec/requests/applications_spec.rb
+++ server/spec/requests/applications_spec.rb
@@ -1,51 +1,70 @@
 require 'rails_helper'
 
-RSpec.describe "Applications", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/applications"
+RSpec.describe 'Applications', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/v1/applications'
       expect(response).to have_http_status(:success)
     end
 
-    it "returns an assign application instances" do
+    it 'returns an assign application instances' do
       get '/api/v1/applications'
-      assigns(:applications).should
+      expect(assigns(:applications)).to_not be(nil)
     end
 
-    it "returns all instances of applications" do
+    it 'returns all instances of applications' do
       get '/api/v1/applications'
       count = Application.all.count
       expect(assigns(:applications).count).to eq(count)
     end
-
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/applications/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      application = Application.create(
+        date: '21/10/2022',
+        job_title: 'test',
+        company: 'test company',
+        application_method: 'direct',
+        response: 'rejection',
+        comment: 'comment'
+      )
+      get "/api/v1/applications/#{application.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'assigns an application instance' do
+      application = Application.create(
+        date: '21/10/2022',
+        job_title: 'test',
+        company: 'test company',
+        application_method: 'direct',
+        response: 'rejection',
+        comment: 'comment'
+      )
+      get "/api/v1/applications/#{application.id}"
+      expect(assigns(:application)).to eq(application)
+    end
+  end
+
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/api/v1/applications/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/applications/create"
+  describe 'GET /update' do
+    it 'returns http success' do
+      get '/api/v1/applications/update'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/api/v1/applications/update"
+  describe 'GET /delete' do
+    it 'returns http success' do
+      get '/api/v1/applications/delete'
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /delete" do
-    it "returns http success" do
-      get "/api/v1/applications/delete"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
