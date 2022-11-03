9c7abd3d4b1d7284d00828f526fba1ea3eb9398c
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-03 10:29:48 +0000

    update routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :applications, only: %i[index show create update delete]
+      resources :job_applications, only: %i[index show create update delete]
     end
   end
 end
