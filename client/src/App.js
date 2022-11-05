97f4d437ff6f4a580c23a53ea2f46d753503cfac
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-05 10:43:05 +0000

    add logo placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Job Application Data</h1>
+      <h1 className="text-center"><img src="logo192.png" alt="" style={{"height": "1em"}}/> Job Application Data</h1>
     </div>
   );
 }
