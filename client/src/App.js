0bae0487c0e2ee7910abb211627e7a1245b2feba
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-05 12:09:55 +0000

    add copy

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,17 @@ function App() {
     <div className="App">
       <h1 className="text-center">
         <img src="logo192.png" alt="" style={{ height: '1em' }} /> Job
-        Application Data
+        Search Visualisation
       </h1>
+
+      <div className='text-center'>
+        <p>
+          Example Sankey Diagram created with{' '}
+          <a href="https://sankeymatic.com/build/">Sankeymatic</a>
+        </p>
+        <p>My job search started on <strong>21st August 2022</strong> and is still ongoing!</p>
+      </div>
+
       <div
         style={{
           width: '800px',
