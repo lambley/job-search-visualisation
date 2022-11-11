459a4b8490e9b0e72eafaaae3fe342aea9800cfb
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-11 10:25:33 +0000

    move styles to scss and add params to ExampleSankey

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,23 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-center">
-        <img src="logo192.png" alt="" style={{ height: '1em' }} /> Job
-        Search Visualisation
+        <img src="logo192.png" alt="" style={{ height: '1em' }} /> Job Search
+        Visualisation
       </h1>
 
-      <div className='text-center'>
+      <div className="text-center">
         <p>
           Example Sankey Diagram created with{' '}
           <a href="https://sankeymatic.com/build/">Sankeymatic</a>
         </p>
-        <p>My job search started on <strong>21st August 2022</strong> and is still ongoing!</p>
+        <p>
+          My job search started on <strong>21st August 2022</strong> and is
+          still ongoing!
+        </p>
       </div>
 
-      <div
-        style={{
-          width: '800px',
-          marginTop: '32px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-      >
-        <ExampleSankey />
+      <div className="center-diagram">
+        <ExampleSankey width={600} height={600} />
       </div>
     </div>
   );
