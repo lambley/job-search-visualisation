f517c634f39ea8fc14a92cafb7b4bf5cc2a1eeab
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-12 09:00:05 +0000

    install react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Diagram from './components/Diagram';
+import List from './components/List';
+import NavMain from './components/NavMain';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './stylesheets/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <NavMain /> 
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/diagram" element={<Diagram />}></Route>
+      <Route path="/list" element={<List />}></Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
 );
