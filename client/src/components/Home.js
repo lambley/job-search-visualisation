44667f48907be9a063c77dde3e371c74834b0559
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-15 13:26:31 +0000

    remove comment

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -34,7 +34,6 @@ const Home = () => {
         </p>
       </div>
       <JobTable columnNames={columnNames} jobApplications={jobApplications} />
-      {/* <ul>{renderJobApplications()}</ul> */}
     </div>
   );
 };
