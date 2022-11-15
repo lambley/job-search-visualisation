e1a1c9f5025a13123c6775b0841303a45fe4f74e
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-15 13:29:05 +0000

    add JobTable component to List

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
+import Api from '../helper/api';
+import JobTable from './JobTable';
+
 const List = () => {
+  const [jobApplications, setJobApplications] = useState([]);
+
+  const columnNames = ['Date', 'Job Title', 'Application Method'];
+
+  const api = new Api();
+  const fetchJobApplications = async () => {
+    const res = await api.index();
+    setJobApplications(res.data);
+  };
+
+  // ensure fetchJobApplications is called once only
+  useEffect(() => {
+    fetchJobApplications();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="text-center">
       <h1>List of current job application data</h1>
-      <p>SHOW TABLE HERE</p>
+      <JobTable columnNames={columnNames} jobApplications={jobApplications} />
     </div>
   );
 };
