09a5a7ad7262b20c25939d2377689fe81a699eb1
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-19 15:06:51 +0000

    create basic formik form

--- client/src/pages/JobNew.js
+++ client/src/pages/JobNew.js
@@ -1,7 +1,33 @@
+import { Formik, Field, Form } from 'formik';
+
 const JobNew = () => {
   return (
-    <div>
+    <div className="text-center">
       <h1>Create new Job Application</h1>
+      <Formik initialValues={{
+        date: '',
+        jobTitle: '',
+        applicationMethod: ''
+      }}
+
+      onSubmit={async(values) => {
+        await console.log(values);
+      }}
+
+      >
+        <Form>
+          <label htmlFor="date">Date</label>
+          <Field id="date" name="date" placeholder=''></Field>
+
+          <label htmlFor="jobTitle">Job Title</label>
+          <Field id="jobTitle" name="jobTitle" placeholder='Software Developer'></Field>
+
+          <label htmlFor="applicationMethod">Application Method</label>
+          <Field id="applicationMethod" name="applicationMethod" placeholder='e.g. Indeed'></Field>
+
+          <button type='submit'>Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
