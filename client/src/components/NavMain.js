1337abb32341dc431c2880fb8984797702244c05
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-19 14:30:34 +0000

    fix link to pages to be absolute rather than relative

--- client/src/components/NavMain.js
+++ client/src/components/NavMain.js
@@ -17,8 +17,8 @@ const NavMain = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="diagram">Diagram</Nav.Link>
-            <Nav.Link href="list">List</Nav.Link>
+            <Nav.Link href="/diagram">Diagram</Nav.Link>
+            <Nav.Link href="/list">List</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
