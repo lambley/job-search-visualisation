f0d942aceb5b1aafc744a945f93bb73a1428b9b1
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-20 12:48:30 +0000

    add basic error validations to form

--- client/src/pages/JobNew.js
+++ client/src/pages/JobNew.js
@@ -1,33 +1,77 @@
 import { Formik, Field, Form } from 'formik';
+import DatePickerField from '../components/DatePickerField';
 
 const JobNew = () => {
+  const validate = (values) => {
+    const errors = {};
+    if (!values.date) {
+      errors.date = '*Required';
+    }
+
+    if (!values.jobTitle) {
+      errors.jobTitle = '*Required';
+    }
+
+    if (!values.applicationMethod) {
+      errors.applicationMethod = '*Required';
+    }
+
+    return errors;
+  };
+
   return (
-    <div className="text-center">
-      <h1>Create new Job Application</h1>
-      <Formik initialValues={{
-        date: '',
-        jobTitle: '',
-        applicationMethod: ''
-      }}
-
-      onSubmit={async(values) => {
-        await console.log(values);
-      }}
-
-      >
-        <Form>
-          <label htmlFor="date">Date</label>
-          <Field id="date" name="date" placeholder=''></Field>
-
-          <label htmlFor="jobTitle">Job Title</label>
-          <Field id="jobTitle" name="jobTitle" placeholder='Software Developer'></Field>
-
-          <label htmlFor="applicationMethod">Application Method</label>
-          <Field id="applicationMethod" name="applicationMethod" placeholder='e.g. Indeed'></Field>
-
-          <button type='submit'>Submit</button>
-        </Form>
-      </Formik>
+    <div>
+      <h1 className="text-center">Create new Job Application</h1>
+      <div className="form-wrapper">
+        <Formik
+          initialValues={{
+            date: new Date(),
+            jobTitle: '',
+            applicationMethod: '',
+          }}
+          validate={validate}
+          onSubmit={(values) => {
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {(props) => (
+            <Form>
+              <label htmlFor="date">Date</label>
+              <DatePickerField name="date" />
+              {props.errors.date && (
+                <div className="form-error">{props.errors.date}</div>
+              )}
+              <label htmlFor="jobTitle">Job Title</label>
+              <Field
+                id="jobTitle"
+                name="jobTitle"
+                type="text"
+                placeholder="Software Developer"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+              ></Field>
+              {props.errors.jobTitle && (
+                <div className="form-error">{props.errors.jobTitle}</div>
+              )}
+              <label htmlFor="applicationMethod">Application Method</label>
+              <Field
+                id="applicationMethod"
+                name="applicationMethod"
+                type="text"
+                placeholder="e.g. Indeed"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+              ></Field>
+              {props.errors.applicationMethod && (
+                <div className="form-error">
+                  {props.errors.applicationMethod}
+                </div>
+              )}
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
